a6714a56d2410423cbd9ca9b600ab89cda52f5ca
Krylov Egor <KrylovWork@yandex.ru>
2022-10-11 17:34:16 +0500

    task 4 solved

---
+++ hw1/4.py
@@ -0,0 +1,12 @@
+print("Введите номер четверти: ")
+n = int(input())
+result = ''
+
+if n == 1 :
+    print("x > 0, y > 0")
+if n == 2:
+    print("x < 0, y > 0")
+if n == 3:
+    print("x < 0, y < 0")
+if n == 4:
+    print("x > 0, y < 0")
