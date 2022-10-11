114c73a766de031171cfe3bebd09e903bbe72e97
Krylov Egor <KrylovWork@yandex.ru>
2022-10-11 17:23:54 +0500

    task 2 solved

--- hw1/2.py
+++ hw1/2.py
@@ -1,1 +1,7 @@
-print("Hello world")
+result = "утверждение верно"
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (not (x and y and z) == (not x) or (not y) or (not z)):
+                result = "утверждение не верно"
+print(result)
