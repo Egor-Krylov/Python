383670356046ff42efbaa60891d63bd69649d73f
Krylov Egor <KrylovWork@yandex.ru>
2022-11-16 18:53:40 +0500

    hw6 done

---
+++ hw6/1.py
@@ -0,0 +1,23 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+
+# Старый код:
+
+# list = [1,1,3,5,2,5,8,9,4,3,4]
+# resultList = []
+# for i in range(len(list)):
+#     duplicate = False
+#     for j in range(len(list)):
+#         if (i != j) and list[i] == list[j]:
+#             duplicate = True
+#     if not duplicate:
+#         resultList.append(list[i])
+# print(resultList)
+
+
+# Новый код:
+
+list = [1,1,3,5,2,5,8,9,4,3,4]
+
+print([i for i in list if list.count(i) == 1])
+
